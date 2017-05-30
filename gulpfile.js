39235faed68d9b299cb83fc1ea9d1b7ad1a1c2ae
Valerii R <vr@gmail.com>
2017-05-30 21:27:23 +0300

    add delivery, guarantee, products, questions, review sections

--- gulpfile.js
+++ gulpfile.js
@@ -98,6 +98,27 @@ gulp.task('build', ['removedist', 'imagemin', 'sass', 'js'], function() {
 
 });
 
+gulp.task('build-with-tinypng', ['removedist', 'tinypng', 'sass', 'js'], function() {
+
+	var buildFiles = gulp.src([
+		'app/*.html',
+		'app/.htaccess',
+		]).pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'app/css/main.min.css',
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src([
+		'app/js/scripts.min.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src([
+		'app/fonts/**/*',
+		]).pipe(gulp.dest('dist/fonts'));
+
+});
+
 gulp.task('deploy', function() {
 
 	var conn = ftp.create({
