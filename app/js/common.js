a10052f826b6b2fe3ea1f5b9fb7b87f68d6dd6cd
Valerii R <vr@gmail.com>
2017-05-31 18:02:23 +0300

    add carousel, mobile responsivity

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,28 @@
 $(function() {
 
 	// Custom JS
-
+	$('.owl-carousel').owlCarousel({
+		margin: 20,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+		responsiveClass: true,
+		responsive:{
+				0:{
+						items:1,
+				},
+				480:{
+						items:2,
+				},
+				768:{
+						items:3,
+				},
+				992:{
+						items:4,
+				},
+				1200:{
+						items:4,
+				}
+		}
+	});
+	
 });
