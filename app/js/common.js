9f2e01c65d68334df4f55aae3d4186a5073119ad
Valerii R <vr@gmail.com>
2017-06-02 12:42:44 +0300

    change responsivity

--- app/js/common.js
+++ app/js/common.js
@@ -1,28 +1,40 @@
 $(function() {
 
 	// Custom JS
-	$('.owl-carousel').owlCarousel({
+
+
+
+
+	$('.all-products').owlCarousel({
 		margin: 20,
 		nav: true,
 		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
 		responsiveClass: true,
 		responsive:{
-				0:{
-						items:1,
-				},
-				480:{
-						items:2,
-				},
-				768:{
-						items:3,
-				},
-				992:{
-						items:4,
-				},
-				1200:{
-						items:4,
-				}
+			0:{
+				items:1,
+			},
+			480:{
+				items:2,
+			},
+			768:{
+				items:3,
+			},
+			992:{
+				items:4,
+			},
+			1200:{
+				items:4,
+			}
 		}
 	});
-	
+
+	$('.all-reviews').owlCarousel({
+		margin: 20,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+		items: 1
+	});
+
+
 });
